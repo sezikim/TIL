65acae44374cb81e881c202e87691e8d97a16bce
kdghj1002 <kdghj1002@naver.com>
2019-03-08 05:11:31 +0000

    C

--- Cpragprog2/prac4.c
+++ Cpragprog2/prac4.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
+void Palindrome(char*, int);
+
 int main(void)
 {
-	int str[30];
+	char str[30];
 	
 	printf("문자열 입력: ");
-	gets(str);
+	scanf("%s", str);
+	Palindrome(str, strlen(str));
+
+	return 0;
 }
+
+void Palindrome(char* str, int len)
+{
+	for(int i=0;i<len;i++)
+		if(str[i]!=str[--len]){
+			printf("화문이 아닙니다.\n");
+			return;
+		}
+		printf("화문 입니다.\n");
+	return;
+}
+
